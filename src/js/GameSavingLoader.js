ff7e6d196f38f53d246f0974e80d80ca05b95b28
a-naraikin <anna.naraikin@yandex.ru>
2021-10-04 00:56:42 +0300

    fix: class GameSavingLoader

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -3,12 +3,8 @@ import read from './reader';
 
 export default class GameSavingLoader {
   static async load() {
-    try {
-      const reader = await read();
-      const parser = await json(reader);
-      return parser;
-    } catch (error) {
-      throw error;
-    }
+    const reader = await read();
+    const parser = await json(reader);
+    return parser;
   }
 }
