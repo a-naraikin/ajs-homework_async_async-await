1cb14c9d58a134c7926b56bcbb7cff5f32074689
a-naraikin <anna.naraikin@yandex.ru>
2021-10-05 22:50:39 +0300

    fix: app.js, class GameSavingLoader and test

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,10 +1,11 @@
 import json from './parser';
 import read from './reader';
+import GameSaving from './GameSaving';
 
 export default class GameSavingLoader {
   static async load() {
     const reader = await read();
     const parser = await json(reader);
-    return parser;
+    return new GameSaving(JSON.parse(parser));
   }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,10 @@
 /* eslint-disable no-console */
 import GameSavingLoader from './GameSavingLoader';
-import GameSaving from './GameSaving';
 
 (async () => {
   try {
     const saving = await GameSavingLoader.load();
-    const gameSaving = new GameSaving(JSON.parse(saving));
-    console.log(gameSaving);
+    console.log(saving);
   } catch (error) {
     console.log(error);
   }
